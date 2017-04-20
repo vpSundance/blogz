d402249c3781a663ef7c9ca8a484bcd1829c4ee7
Kaley Schenk <kaley.schenk@gmail.com>
2017-04-20 17:07:34 -0500

    User post filter

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ class BlogHandler(Handler):
             The user parameter will be a User object.
         """
 
-        # TODO - filter the query so that only posts by the given user
-        return None
+        query =  Post.all().order('-created').filter('author = ', user)
+        return query.fetch(limit=limit, offset=offset)
 
     def get_user_by_name(self, username):
         """ Get a user object from the db, based on their username """
